52115031b317dc8cf1b8be920dc1843a4e9d5110
Billones142 <84104251+Billones142@users.noreply.github.com>
2023-09-20 17:41:58 -0300

    Add files via upload

---
+++ src/main/java/email/ucp/classes/User.java
@@ -0,0 +1,5 @@
+package email.ucp.classes;
+
+public class User {
+    
+}
---
+++ src/test/java/email/ucp/UserTest.java
@@ -0,0 +1,16 @@
+package email.ucp;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import email.ucp.classes.User;
+
+public class UserTest {
+    @Test
+    public void test(){
+        User usuario= new User();
+
+        assertNotEquals(null , usuario);
+    }
+}
